8f2a3857064bd7c76d718d081ba6a7e210ab3da1
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2015-12-17 00:26:03 +0300

    CSV provider was implemented

---
+++ src/Contract/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackPackOptimizer.Contract
+{
+    public struct Purchase
+    {
+        public string SourceNamr { get; set; }
+        public short NumberOfGallons { get; set; }
+        public float PriceOfGallon { get; set; }
+    }
+}
--- src/DataProviders/DataProviders.Contract/IMerchendiseProvider.cs
+++ src/DataProviders/DataProviders.Contract/IMerchendiseProvider.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsvProvider
+namespace DataProviders.Contract
 {
-    public class Class1
+    public interface IMerchendiseProvider
     {
+        IEnumerable<Merchendise> Merchendises { get; }
     }
 }
